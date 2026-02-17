0e5d7ec56cbfc4ac71564ffa71346fc29974c63e
Abhishek882772 <abhishek.23b1541075@abes.ac.in>
2026-02-17 11:34:28 +0530

    new

--- os.js
+++ os.js
@@ -5,3 +5,5 @@ console.log("system architecture: "+os.arch());
 console.log("os memory:" +os.totalmem());
 console.log("os free memory:" +os.freemem());
 console.log("os uptime:" +os.uptime());
+console.log("home directory:" +os.homedir());
+console.log("host name:" +os.hostname());
