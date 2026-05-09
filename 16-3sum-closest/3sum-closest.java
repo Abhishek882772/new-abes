699014abcd51d7a59c5ea479127134682a4f9d1f
Abhishek Tripathi <abhishek.23b1541075@abes.ac.in>
2026-05-10 00:45:33 +0530

    Time: 17 ms (77.93%) | Memory: 45.5 MB (66.20%) - LeetSync

--- 16-3sum-closest/3sum-closest.java
+++ 16-3sum-closest/3sum-closest.java
@@ -5,13 +5,15 @@ class Solution {
         Arrays.sort(nums);
         int n=nums.length;
         for(int i=0;i<n-2;i++){
-            for(int j=i+1;j<n-1;j++){
-                for(int k=j+1;k<n;k++){
+            int j=i+1;
+            int k=n-1;
+            while(j<k){
                     int ans=nums[i]+nums[j]+nums[k];
                     int s=Math.abs(target-(nums[i]+nums[j]+nums[k]));
-                    if(s==0) { return target;}
                     if(c>s) {sum=ans; c=s;}
-                }
+                    if(ans>target) k--;
+                    else if(ans<target) j++;
+                    else return ans;
             }
         }
         return sum;
