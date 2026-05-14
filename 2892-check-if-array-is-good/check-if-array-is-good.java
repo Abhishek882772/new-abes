a21bb7605bfa95ee02076be37752cde0b1ab51a8
Abhishek Tripathi <abhishek.23b1541075@abes.ac.in>
2026-05-14 23:57:20 +0530

    Time: 1 ms (91.05%) | Memory: 43.8 MB (99.24%) - LeetSync

--- 2892-check-if-array-is-good/check-if-array-is-good.java
+++ 2892-check-if-array-is-good/check-if-array-is-good.java
@@ -1,11 +1,15 @@
 class Solution {
     public boolean isGood(int[] nums) {
-        Arrays.sort(nums);
-        if(nums.length != nums[nums.length-1]+1) return false;
-        if(nums[nums.length-1] != nums[nums.length-2]) return false;
-        for(int i=0;i<nums.length-1;i++){
-            if(nums[i] != i+1) return false;
+       int max=nums.length-1;
+       int[] frr=new int [nums.length-1];
+        for(int i=0;i<nums.length;i++){
+            if(nums[i] > max || nums[i]<1) return false;
+            frr[nums[i]-1] ++;
         }
+        for(int i=0;i<frr.length-1;i++){
+            if(frr[i] != 1) return false;
+        }
+        if(frr[frr.length-1] != 2) return false;
         return true;
     }
 }
