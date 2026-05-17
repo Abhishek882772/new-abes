fb7f4710d88fb6319bff184ec207103899e10594
Abhishek Tripathi <abhishek.23b1541075@abes.ac.in>
2026-05-18 00:17:57 +0530

    Time: 1 ms (99.73%) | Memory: 52.2 MB (51.59%) - LeetSync

--- 1428-jump-game-iii/jump-game-iii.java
+++ 1428-jump-game-iii/jump-game-iii.java
@@ -1,15 +1,8 @@
 class Solution {
     public boolean canReach(int[] arr, int start) {
-        if(arr[start]==0) return true;
-        boolean[] bool=new boolean[arr.length];
-        return giveindex(arr,start,bool);
-    }
-    private boolean giveindex(int[] arr,int start,boolean[] bool){
-        if(arr[start]==0) return true;
-        if(bool[start]) return false;
-        bool[start]=true;
-           if(start+arr[start]<arr.length) if (giveindex(arr,start+arr[start], bool)) return true;
-           if(start-arr[start]>=0) if (giveindex(arr,start-arr[start], bool)) return true;
-        return false;
+       if(start >= arr.length || start <0 || arr[start]<0) return false;
+       if(arr[start]==0) return true;
+       arr[start]=-arr[start];
+       return (canReach(arr,start+arr[start]) || canReach(arr,start-arr[start]));
     }
 }
