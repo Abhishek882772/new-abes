a2b22649cbf473dca690a672687b26d8c1c8ec0f
Abhishek Tripathi <abhishek.23b1541075@abes.ac.in>
2026-05-18 00:11:56 +0530

    Time: 2 ms (82.42%) | Memory: 52.8 MB (33.20%) - LeetSync

--- 1428-jump-game-iii/jump-game-iii.java
+++ 1428-jump-game-iii/jump-game-iii.java
@@ -1,28 +1,15 @@
 class Solution {
     public boolean canReach(int[] arr, int start) {
         if(arr[start]==0) return true;
-        int n=arr.length;
-        List<List<Integer>> list=new ArrayList<>();
-        for(int i=0;i<arr.length;i++){
-            List<Integer> li=new ArrayList<>();
-            if(arr[i]==0){
-                li.add(0);
-               }
-            if(i+arr[i]<n) li.add(i+arr[i]);
-            if(i-arr[i]>=0)li.add(i-arr[i]);
-            list.add(li);
-        }
         boolean[] bool=new boolean[arr.length];
-        return giveindex(arr,start,list,bool);
+        return giveindex(arr,start,bool);
     }
-    private boolean giveindex(int[] arr,int start,List<List<Integer>> list,boolean[] bool){
+    private boolean giveindex(int[] arr,int start,boolean[] bool){
         if(arr[start]==0) return true;
         if(bool[start]) return false;
         bool[start]=true;
-        for(int i=0;i<list.get(start).size();i++){
-            if (giveindex(arr,list.get(start).get(i), list, bool))
-                return true;
-        }
+           if(start+arr[start]<arr.length) if (giveindex(arr,start+arr[start], bool)) return true;
+           if(start-arr[start]>=0) if (giveindex(arr,start-arr[start], bool)) return true;
         return false;
     }
 }
