6b6de7d9bb1d11596842d4ac70aacdc5fdfb6392
Abhishek Tripathi <abhishek.23b1541075@abes.ac.in>
2026-06-03 00:33:06 +0530

    Time: 1 ms (100.00%) | Memory: 46.5 MB (95.24%) - LeetSync

---
+++ 3965-earliest-finish-time-for-land-and-water-rides-i/earliest-finish-time-for-land-and-water-rides-i.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int earliestFinishTime(int[] landS, int[] landD, int[] waterS, int[] waterD) {
+
+      int min1=100000;
+      int min2=100000;
+      int ans=1000000;
+      for(int i=0;i<landS.length;i++){
+        min1=Math.min(min1,landS[i]+landD[i]);
+      }
+      for(int i=0;i<waterS.length;i++){
+        if(min1>waterS[i]) ans=Math.min(ans,min1+waterD[i]);
+        else ans=Math.min(ans,waterS[i]+waterD[i]);
+        min2=Math.min(min2,waterS[i]+waterD[i]);
+      }
+      for(int i=0;i<landS.length;i++){
+        if(min2>landS[i]) ans=Math.min(ans,min2+landD[i]);
+        else ans=Math.min(ans,landS[i]+landD[i]);
+      }
+      return ans;
+    }
+}
