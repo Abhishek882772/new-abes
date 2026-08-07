3a7a883173a67e354e89d5decf28b68051091b1f
Abhishek Tripathi <abhishek.23b1541075@abes.ac.in>
2026-08-07 11:15:05 +0530

    Time: 7 ms (39.94%) | Memory: 46.4 MB (80.89%) - LeetSync

--- 1222-remove-covered-intervals/remove-covered-intervals.java
+++ 1222-remove-covered-intervals/remove-covered-intervals.java
@@ -4,7 +4,7 @@ class Solution {
         int count=1;
         int i=0; int j=1;
         Arrays.sort(intervals,(a,b)->a[0]!=b[0]?a[0]-b[0]:b[1]-a[1]);
-        int count1=1;
+        
         if(intervals.length <=1 ) return intervals.length;
         while(j < intervals.length){
              if(intervals[i][0] <= intervals[j][0] && intervals[i][1] >= intervals[j][1] ){
