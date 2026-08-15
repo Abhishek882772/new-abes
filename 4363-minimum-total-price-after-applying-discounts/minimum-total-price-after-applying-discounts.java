4d550d172fa279353726d7f70778322e0225cbe1
Abhishek Tripathi <abhishek.23b1541075@abes.ac.in>
2026-08-15 11:05:12 +0530

    Time: 46 ms (34.78%) | Memory: 146 MB (23.14%) - LeetSync

---
+++ 4363-minimum-total-price-after-applying-discounts/minimum-total-price-after-applying-discounts.java
@@ -0,0 +1,18 @@
+class Solution {
+    public double minPrice(int[] prices, int[] discounts) {
+        int j=discounts.length-1;
+        int i=prices.length-1;
+        Arrays.sort(prices);
+        Arrays.sort(discounts);
+        double ans=0;
+        while(i>=0){
+            if(j>=0){
+                ans+=((double)(prices[i]*(100-discounts[j]))/100);
+                j--;
+            }
+            else ans+=prices[i];
+            i--;
+        }
+        return ans;
+    }
+}
