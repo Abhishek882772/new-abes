c0d5864dc9fde7c9f8738245154cf5dc637ac039
Abhishek Tripathi <abhishek.23b1541075@abes.ac.in>
2026-08-16 09:58:08 +0530

    Time: 7 ms (29.05%) | Memory: 114.7 MB (82.43%) - LeetSync

---
+++ 2156-stone-game-ix/stone-game-ix.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean stoneGameIX(int[] stones) {
+
+        int c0 = 0;
+        int c1 = 0;
+        int c2 = 0;
+
+        // Count stones according to remainder when divided by 3
+        for (int stone : stones) {
+            int rem = stone % 3;
+
+            if (rem == 0) {
+                c0++;
+            } else if (rem == 1) {
+                c1++;
+            } else {
+                c2++;
+            }
+        }
+
+        // Case 1: Even number of remainder-0 stones
+        if (c0 % 2 == 0) {
+            return c1 > 0 && c2 > 0;
+        }
+
+        // Case 2: Odd number of remainder-0 stones
+        return Math.abs(c1 - c2) > 2;
+    }
+}
