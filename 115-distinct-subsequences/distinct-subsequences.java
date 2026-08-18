0e443033da2966f68eb93c6046cf226d5001fa2a
Abhishek Tripathi <abhishek.23b1541075@abes.ac.in>
2026-08-18 19:03:23 +0530

    Time: 22 ms (24.00%) | Memory: 54.2 MB (64.14%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -1,17 +1,15 @@
 class Solution {
     public int numDistinct(String s, String t) {
-        int[][] dp=new int[s.length()][t.length()];
-        for(int i=0;i<s.length();i++) Arrays.fill(dp[i],-1);
-        return gettheno(s,t,0,0,dp);
-    }
-    private int gettheno(String s, String t,int i,int j,int[][] dp){
-        if(j>=t.length()) return 1;
-        if(i>=s.length()) return 0;
-        if(dp[i][j] != -1) return dp[i][j];
-        if(s.charAt(i)==t.charAt(j)){
-            return dp[i][j]=gettheno(s,t,i+1,j+1,dp)+gettheno(s,t,i+1,j,dp);
+        int[][] dp=new int[s.length()+1][t.length()+1];
+        for(int j=0;j<=s.length();j++) dp[j][0]=1;
+        for(int i=1;i<=s.length();i++){
+            for(int j=1;j<=t.length();j++){
+                if(s.charAt(i-1)==t.charAt(j-1)){
+                    dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
+                }
+                else dp[i][j]=dp[i-1][j];
+            }
         }
-        else return dp[i][j]=gettheno(s,t,i+1,j,dp);
-         
+    return dp[s.length()][t.length()];
     }
 }
