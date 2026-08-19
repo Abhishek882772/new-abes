72077d50b1738987ff9ed69b25bf9ca052dee4c1
Abhishek Tripathi <abhishek.23b1541075@abes.ac.in>
2026-08-19 17:43:21 +0530

    Time: 15 ms (98.68%) | Memory: 53.5 MB (32.63%) - LeetSync

--- 1487-cinema-seat-allocation/cinema-seat-allocation.java
+++ 1487-cinema-seat-allocation/cinema-seat-allocation.java
@@ -1,66 +1,57 @@
 class Solution {
     public int maxNumberOfFamilies(int n, int[][] reservedSeats) {
+        // Store reserved seats for each row
+        Map<Integer, Integer> reserved = new HashMap<>();
 
-        Arrays.sort(reservedSeats, (a, b) -> a[0] - b[0]);
+        for (int[] seat : reservedSeats) {
+            int row = seat[0];
+            int col = seat[1];
 
-        int ans = 0;
-        int i = 0;
+            // Only seats 2 to 9 matter
+            if (col >= 2 && col <= 9) {
+                reserved.put(row, reserved.getOrDefault(row, 0) | (1 << col));
+            }
+        }
 
-        while (i < reservedSeats.length) {
+        // Every completely empty row can hold 2 families
+        int answer = (n - reserved.size()) * 2;
 
-            int row = reservedSeats[i][0];
+        for (int mask : reserved.values()) {
+            boolean left = true;   // seats 2-5
+            boolean middle = true; // seats 4-7
+            boolean right = true;  // seats 6-9
 
-            boolean left = true;    // 2,3,4,5
-            boolean middle = true;  // 4,5,6,7
-            boolean right = true;   // 6,7,8,9
-
-            // Process all reserved seats of this row
-            while (i < reservedSeats.length &&
-                   reservedSeats[i][0] == row) {
-
-                int seat = reservedSeats[i][1];
-
-                if (seat >= 2 && seat <= 5) {
+            // Check seats 2,3,4,5
+            for (int seat = 2; seat <= 5; seat++) {
+                if ((mask & (1 << seat)) != 0) {
                     left = false;
+                    break;
                 }
+            }
 
-                if (seat >= 4 && seat <= 7) {
+            // Check seats 4,5,6,7
+            for (int seat = 4; seat <= 7; seat++) {
+                if ((mask & (1 << seat)) != 0) {
                     middle = false;
+                    break;
                 }
+            }
 
-                if (seat >= 6 && seat <= 9) {
+            // Check seats 6,7,8,9
+            for (int seat = 6; seat <= 9; seat++) {
+                if ((mask & (1 << seat)) != 0) {
                     right = false;
+                    break;
                 }
-
-                i++;
             }
 
-            // Both non-overlapping blocks are available
             if (left && right) {
-                ans += 2;
-            }
-            // At least one block is available
-            else if (left || middle || right) {
-                ans += 1;
+                answer += 2;
+            } else if (left || middle || right) {
+                answer += 1;
             }
         }
 
-        // Rows having no reserved seats
-        int reservedRows = 0;
-
-        for (int j = 0; j < reservedSeats.length; ) {
-            reservedRows++;
-
-            int row = reservedSeats[j][0];
-
-            while (j < reservedSeats.length &&
-                   reservedSeats[j][0] == row) {
-                j++;
-            }
-        }
-
-        ans += (n - reservedRows) * 2;
-
-        return ans;
+        return answer;
     }
 }
