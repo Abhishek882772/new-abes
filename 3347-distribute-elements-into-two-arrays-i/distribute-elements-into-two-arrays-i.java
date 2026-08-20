7a2fd7e0aa7ee271d8b64df4258aff746a33a45c
Abhishek Tripathi <abhishek.23b1541075@abes.ac.in>
2026-08-20 10:17:05 +0530

    Time: 1 ms (98.08%) | Memory: 46.7 MB (42.79%) - LeetSync

--- 3347-distribute-elements-into-two-arrays-i/distribute-elements-into-two-arrays-i.java
+++ 3347-distribute-elements-into-two-arrays-i/distribute-elements-into-two-arrays-i.java
@@ -1,28 +1,23 @@
 class Solution {
     public int[] resultArray(int[] nums) {
-        Stack<Integer> st1=new Stack<>();
-        Stack<Integer> st2=new Stack<>();
-        st1.push(nums[0]);
-        st2.push(nums[1]);
+        ArrayList<Integer> a=new ArrayList<>();
+        ArrayList<Integer> b=new ArrayList<>();
+        a.add(nums[0]);
+        b.add(nums[1]);
         for(int i=2;i<nums.length;i++){
-            if(st1.peek()>st2.peek()) st1.push(nums[i]);
-            else st2.push(nums[i]);
-        } Stack<Integer> st3=new Stack<>();
-        int[] arr=new int[nums.length];
-        while(st1.size()>0){
-            st3.push(st1.pop());
-        }int i=0;
-        while(st3.size()>0){
-            arr[i]=st3.pop();
+            if(a.get(a.size()-1)>b.get(b.size()-1)) a.add(nums[i]);
+            else b.add(nums[i]);
+        }
+        int i=0;
+        for(int k=0;k<a.size();k++) {
+            nums[i]=a.get(k);
             i++;
         }
-        while(st2.size()>0){
-            st3.push(st2.pop());
-        }
-        while(st3.size()>0){
-            arr[i]=st3.pop();
+        for(int k=0;k<b.size();k++){
+            nums[i]=b.get(k);
             i++;
         }
-        return arr;
+        return nums;
+
     }
 }
