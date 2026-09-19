e3ebea2a75de4f46875563ba2926e24f2d762a16
Abhishek Tripathi <abhishek.23b1541075@abes.ac.in>
2026-09-19 10:52:34 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (27.28%) - LeetSync

---
+++ 226-invert-binary-tree/invert-binary-tree.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private void invert(TreeNode root){
+       if(root==null || (root.left==null && root.right==null)) return ;
+       TreeNode swp=root.left;
+        root.left=root.right;
+        root.right=swp;
+        invert(root.left);
+        invert(root.right);
+    }
+    public TreeNode invertTree(TreeNode root) {
+        if(root==null || (root.left==null && root.right==null)) return root;
+        invert(root);
+        return  root;
+    }
+}
