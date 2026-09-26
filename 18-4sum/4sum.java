e580ed7b55c62ac2d024fa21a4802b56b0dcabde
Abhishek Tripathi <abhishek.23b1541075@abes.ac.in>
2026-09-26 20:53:24 +0530

    Time: 16 ms (93.18%) | Memory: 45.9 MB (46.81%) - LeetSync

--- 18-4sum/4sum.java
+++ 18-4sum/4sum.java
@@ -1,23 +1,39 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        List<List<Integer>> list=new ArrayList<>();
         Arrays.sort(nums);
-        for(int i=0;i<nums.length-3;i++){
-            for(int j=i+1;j<nums.length-2;j++){
-                int k=j+1;
-                int l=nums.length-1;
-                while(k<l){
-                    long sum=(long)nums[i]+nums[j]+nums[k]+nums[l];
-                    if(sum==(long)target){ 
-                        List<Integer> temp=Arrays.asList(nums[i],nums[j],nums[k],nums[l]);
-                        if(! list.contains(temp))list.add(temp);
-                        k++; l--;
-                        }
-                    else if(sum<(long)target) k++;
-                    else if(sum>(long)target) l--;
+        List<List<Integer>> res = new ArrayList<>();
+
+        for(int i=0;i<nums.length;i++){
+            if(i>0 && nums[i]==nums[i-1]){
+                continue;
+            }
+            for(int j=i+1;j<nums.length;j++){
+                if(j>i+1 && nums[j]==nums[j-1]){
+                    continue;
+                }
+
+                long sum = nums[i]+nums[j];
+                int l = j+1;
+                int r = nums.length-1;
+                while(l!=j && r!=j && l<r){
+                    long total = sum+nums[l]+nums[r];
+                    if(total>target) r--;
+                    else if(total<target) l++;
+                    else{
+                        List<Integer> li = new ArrayList<>();
+                        li.add(nums[i]);
+                        li.add(nums[j]);
+                        li.add(nums[l]);
+                        li.add(nums[r]);
+                        res.add(li);
+                        while(l<r && nums[l]==nums[l+1]) l++;
+                        while(l<r && nums[r]==nums[r-1]) r--;
+                        l++;
+                        r--;
+                    }
                 }
             }
         }
-        return list;
-    }
+       return res;
+    } 
 }
